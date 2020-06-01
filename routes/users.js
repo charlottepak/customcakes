d1e7e140e3bb3559991c4c05aa6805baa88a3f9a
Charlotte Pak <charlottepak@outlook.com>
2020-05-31 17:28:24 -0700

    create buttons

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var usersCtrl = require('../controllers/users');
+
+router.get('/users', usersCtrl.index);
+router.post(isLoggedIn);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+function isLoggedIn(req, res, next) {
+  if ( req.isAuthenticated() ) {
+    return next();
+  } else {
+    res.redirect('/auth/google');
+  }
+}
 
 module.exports = router;
